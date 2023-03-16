89e08af7e2d435e1f6a5159ec8b37db362ec51af
cod3d-dev <codelgado@gmail.com>
2023-03-16 08:09:22 -0400

    Limpieza rutas

--- routes/web.php
+++ routes/web.php
@@ -25,37 +25,3 @@ Route::get('/', function () {
 Route::get('comandas/ultima', [ComandaControlador::class, 'ultima'])->name('comandas/ultima');
 Route::resource('comandas', ComandaControlador::class);
 
-
-// Route::get('/pedido/nuevo', function () {
-//     return view('pedidos.nuevo');
-    
-// });
-
-
-
-Route::get('/ultimacomanda', function () {
-    // return view('welcome');
-    $fecha = time();
-
-    $totalcomandas = Redis::get('totalcomandas');
-
-    // dd($totalcomandas);
-    
-    $fecha = Redis::hget('comandas:' . $totalcomandas, 'fecha');
-    $cliente = Redis::hget('comandas:' . $totalcomandas, 'cliente');
-
-    $nuevafecha = date('Y-m-d H:i:s', $fecha);
-    $timezone_from = 'America/Caracas';
-    $newDateTime = new DateTime($nuevafecha); 
-    $newDateTime->setTimezone(new DateTimeZone($timezone_from)); 
-    $dateTimeUTC = $newDateTime->format("Y-m-d h:i A");
-    //$fecha2 = new DateTime($fecha);
-    
-    // $visits = Redis::incrby('visits.5.downloads',5);
-
-    echo $dateTimeUTC;
-});
-
-// Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
